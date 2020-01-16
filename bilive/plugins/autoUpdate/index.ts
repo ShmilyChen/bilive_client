f4a06a263353155637c85d66b9b4be9e47de050e
ShmilyLtt <1053778582@qq.com>
2020-01-16 21:18:45 +0800

    添加自动更新选择框

--- bilive/plugins/autoUpdate/index.ts
+++ bilive/plugins/autoUpdate/index.ts
@@ -11,14 +11,21 @@ class AutoUpdate extends Plugin {
   public description = '自动更新本地git，配合pm2可实现自动更新'
   public version = '0.0.3'
   public author = 'Vector000'
-  public async load() {
+  public async load({ defaultOptions, whiteList }: { defaultOptions: options, whiteList: Set<string> }) {
+    defaultOptions.advConfig['checkUpdate'] = false
+    defaultOptions.info['checkUpdate'] = {
+      description: '自动更新',
+      tip: '自动更新bilive_client',
+      type: 'boolean'
+    }
+    whiteList.add('checkUpdate')
     this.loaded = true
   }
-  public async start({}, newUser: boolean) {
-    if (!newUser) this._checkForUpdate()
+  public async start( options: options, newUser: boolean) {
+    if (!newUser) this._checkForUpdate(options)
   }
-  public async loop({ cstMin, cstHour }: { cstMin: number, cstHour: number }) {
-    if (cstMin === 0 && cstHour % 12 === 6) this._checkForUpdate()
+  public async loop({ cstMin, cstHour,options }: { cstMin: number, cstHour: number , options: options}) {
+    if (cstMin === 0 && cstHour % 12 === 6) this._checkForUpdate(options)
   }
   // 根目录路径
   private _dirname = __dirname + '/../../..'
@@ -69,7 +76,8 @@ class AutoUpdate extends Plugin {
    * @private
    * 
    */
-  private async _checkForUpdate() {
+  private async _checkForUpdate( options: options) {
+    if (!<boolean>options.advConfig['checkUpdate']) return
     const pathStatus = await this._checkPath(this._dirname)
     if (!pathStatus) return tools.Log(`未发现git仓库，无法进行自动更新`)
     else {
